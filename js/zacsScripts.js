4c4e4c620f94cea60bcf636c73318e5f9cd0bfe2
zacharytay1994 <s10179991@connect.np.edu.sg>
2019-01-23 23:33:24 +0800

    done

---
+++ js/zacsScripts.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+function on1() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById('sciname').innerHTML = 'Alpinia aquatica (Retz.) Roscoe.';
+    document.getElementById('sciexplanation').innerHTML = 'It grows in sandy beaches and swamp forests. It is found locally in Pulau Semakau and widely planted in HortPark, Kent Ridge Park and Singapore Botanic Gardens (Ginger Garden).';
+}
+
+function on2() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById('sciname').innerHTML = 'Aeschynanthus pulcher';
+    document.getElementById('sciexplanation').innerHTML = 'Bright red, tubular flowers are surrounded at the base by a dark red, tubular calyx (collective group of sepals that forms the outermost layer of the flower). The outer surface of the calyx and corolla (collective group of petals which are fused together to form a tube in this species) are densely covered in short, white hairs. The flowers are vertically oriented and arranged in small clusters at the stem tips.';
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
