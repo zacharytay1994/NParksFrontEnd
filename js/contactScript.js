16a44a07fb76b928fc30b7d681464f6e9979f960
Zheng Yang <s10178386@connect.np.edu.sg>
2019-01-23 23:59:13 +0800

    Contact us done

---
+++ js/contactScript.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+function submit()
+{
+    document.getElementById("submit");
+    alert("Details submitted!");
+}
+
+function reset()
+{
+    document.getElementById("contactForm").reset();
+}
+
